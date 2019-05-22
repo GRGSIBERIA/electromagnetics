22d11ff2fd6c5ad49b0a1a11286a54f0b1f47149
grgsiberia <nanashi4129@gmail.com>
2019-05-22 16:22:28 +0900

    ファイルの存在を確認

---
+++ electromagnetics/FileData.f90
@@ -0,0 +1,34 @@
+﻿    module FileDataModule
+        implicit none
+    
+        type FileData
+            integer :: fd, size, numofLines, maxLength
+            character, dimension(:,:), allocatable :: lines
+        end type
+        
+    contains
+    
+    type(FileData) function init_FileData(fd, path) result(file)
+        implicit none
+        integer, intent(in) :: fd
+        character*(*), intent(in) :: path
+        
+        logical exists_status
+        character, dimension(:), allocatable :: stream
+        
+        ! ファイルを開く前に存在を確認する        
+        file%fd = fd
+        inquire(file=path, exist=exists_status)
+        if (exists_status == .FALSE.) then
+            PRINT *, "File not found: ", path
+            STOP
+        end if
+        
+        
+        OPEN(fd, file=path, status="old")
+        inquire(fd, size=file%size)
+        
+        CLOSE(fd)
+    end function
+        
+    end module
---
+++ electromagnetics/electromagnetics.f90
@@ -0,0 +1,10 @@
+﻿
+    program ElectroMagnetics
+    use FileDataModule
+    implicit none
+
+    type(FileData) file
+    file = init_FileData(20, "E:\temp\abaqus.rpt")
+    
+    end program ElectroMagnetics
+
