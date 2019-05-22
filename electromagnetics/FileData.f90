90db59db7035296e28773d75c15ec0757c2dd9f4
grgsiberia <nanashi4129@gmail.com>
2019-05-22 17:51:10 +0900

    ファイルから文字列データを読み取る

--- electromagnetics/FileData.f90
+++ electromagnetics/FileData.f90
@@ -2,31 +2,60 @@
         implicit none
     
         type FileData
-            integer :: fd, size, numofLines, maxLength
+            integer :: fd, size, numof_lines, max_length
             character, dimension(:,:), allocatable :: lines
         end type
         
     contains
     
     type(FileData) function init_FileData(fd, path) result(file)
+        use FileUtil
         implicit none
         integer, intent(in) :: fd
-        character*(*), intent(in) :: path
+        character(*), intent(in) :: path
         
-        logical exists_status
-        character, dimension(:), allocatable :: stream
+        character(1024) stream
+        integer count, word
         
         ! ファイルを開く前に存在を確認する        
         file%fd = fd
-        inquire(file=path, exist=exists_status)
-        if (exists_status == .FALSE.) then
-            PRINT *, "File not found: ", path
-            STOP
-        end if
+        file%numof_lines = 1
+        file%max_length = 0
+        CALL Exists(path)
         
+        ! ファイルサイズだけ調べてファイルを開く
+        inquire(file=path, size=file%size)
+        OPEN(fd, file=path, status="old", blocksize=1024, buffercount=1024)
         
-        OPEN(fd, file=path, status="old")
-        inquire(fd, size=file%size)
+        ! 行数をカウントする
+        do
+            READ (fd, "(A)", end=100), stream
+            
+            if (file%max_length < len_trim(stream)) then
+                file%max_length = len_trim(stream)
+            end if
+            
+            file%numof_lines = file%numof_lines + 1
+        end do
+100     continue
+        
+        ! 行の長さと行数に対応したデータを確保
+        ALLOCATE(file%lines(file%max_length, file%numof_lines))
+        REWIND(file%fd)
+        
+        ! ファイルから読み取って突っ込む
+        count = 1
+        do
+            READ (fd, "(A)", end=200), stream
+            
+            ! streamの文字列を行データに転送する
+            do word = 1, file%max_length
+                file%lines(word,count) = stream(word:word)
+            end do
+            
+            count = count + 1
+        end do
+200     continue        
         
         CLOSE(fd)
     end function
---
+++ electromagnetics/FileUtil.f90
@@ -0,0 +1,19 @@
+﻿    module FileUtil
+    implicit none
+    contains
+    
+    ! ファイルの存在を確認する
+    subroutine Exists(path)
+        implicit none
+        character*(*), intent(in) :: path
+        logical exists_status
+        
+        inquire(file=path, exist=exists_status)
+        if (exists_status == .FALSE.) then
+            PRINT *, "File not found: ", path
+            STOP
+        end if
+        
+    end subroutine
+    
+    end module
